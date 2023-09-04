7074ff039b3862c4e457720e4faa0323bd6063d7
NawfalDzikra <nawfaldzikra1611@gmail.com>
2023-09-04 11:41:52 +0700

    Create operatorAritmatika.c

---
+++ alpro2311/forum/session5/operatorAritmatika.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+ 
+    int x1 = 4;
+    int x2 = 1;
+    int x3 = 1;
+    int x4 = 1;
+
+    for (int i = 0; i < 5; i++) {
+        a -= 4;
+        a += x1;
+        a *= x2;
+        a /= x3;
+        a %= x4;
+    }
+ 
+    printf("Nilai a setelah loop: %d\n", a);
+
+    return 0;
+}
